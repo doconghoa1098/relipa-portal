750b95269b05095d128ca294413ce330b22c32d7
Tran Thanh Long <longtt@vnext.vn>
2022-06-09 10:27:08 +0700

    updata

--- routes/api.php
+++ routes/api.php
@@ -29,9 +29,7 @@ Route::prefix('/members')->middleware(['checkAuth'])->group(function () {
     Route::get('/edit', [MemberController::class, 'show'])->name('members.edit');
     Route::put('/update', [MemberController::class, 'update'])->name('members.update');
 
-    Route::get('/register-ot/{id}', [RegisterOTController::class, 'viewRegisterOT'])->name('register-ot.view');
-    Route::post('/register-ot/{id}', [RegisterOTController::class, 'createRegisterOT'])->name('register-ot.create');
-    Route::put('/register-ot/edit/{id}', [RegisterOTController::class, 'updateRegisterOT'])->name('register-ot.update');
+
 });
 
 Route::prefix('/worksheets')->middleware(['checkAuth'])->group(function () {
@@ -42,5 +40,6 @@ Route::prefix('/worksheets')->middleware(['checkAuth'])->group(function () {
     Route::post('/register-late-early/create', [RegisterLateEarlyController::class, 'createRegisterLateEarly'])->name('register-late-early.create');
     Route::put('/register-late-early/update', [RegisterLateEarlyController::class, 'updateRegisterLateEarly'])->name('register-late-early.update');
 
-
+    Route::post('/register-ot/create', [RegisterOTController::class, 'createRegisterOT'])->name('register-ot.create');
+    Route::put('/register-ot/update', [RegisterOTController::class, 'updateRegisterOT'])->name('register-ot.update');
 });
